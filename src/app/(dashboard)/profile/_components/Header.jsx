0fe5b6c7d6fc4fb5bcc34e15dc329616a6d38a41
salarGholami <salargholami021@gmail.com>
2024-12-31 19:10:46 +0330

    Create Drawer

--- src/app/(dashboard)/profile/_components/Header.jsx
+++ src/app/(dashboard)/profile/_components/Header.jsx
@@ -3,16 +3,18 @@ import Link from "next/link";
 import ButtonIcon from "@/components/ui/ButtonIcon";
 import Avatar from "@/components/ui/Avatar";
 import { useAuth } from "@/context/AuthContext";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 function Header({}) {
-  // const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
   const { user, isLoading } = useAuth();
   return (
     <header
       className={`bg-secondary-0 ${isLoading ? "bg-opacity-30 blur-md" : ""}`}
     >
       <div className="flex items-center justify-between py-5 px-4 lg:px-8">
-        <div className="flex items-center gap-x-3">
+        {/* <div className="flex items-center gap-x-3">
           <div className="flex flex-col lg:flex-row justify-start lg:items-center gap-x-2">
             <span className="text-sm lg:text-lg font-bold text-secondary-700">
               سلام؛ {user?.name}
@@ -28,7 +30,20 @@ function Header({}) {
               <Avatar src={user?.avatarUrl} />
             </ButtonIcon>
           </Link>
-        </div>
+        </div> */}
+        <ButtonIcon
+          className="block lg:hidden border-none"
+          variant="outline"
+          onClick={() => setIsOpenDrawer(!isOpenDrawer)}
+        >
+          {isOpenDrawer ? <XMarkIcon /> : <Bars3Icon />}
+        </ButtonIcon>
+        <span className="text-sm lg:text-lg font-bold text-secondary-700">
+          سلام؛ {user?.name}
+        </span>
+        <Link href="/profile">
+          <Avatar src={user?.avatarUrl} />
+        </Link>
       </div>
     </header>
   );
