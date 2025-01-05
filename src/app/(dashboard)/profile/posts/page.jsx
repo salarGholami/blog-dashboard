d8c2da2d55a037198bbd4ad19e9d999b7805815d
salarGholami <salargholami021@gmail.com>
2025-01-05 16:15:38 +0330

    what is pagination

--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -4,9 +4,12 @@ import { Spinner } from "@/components/ui/Spinner";
 import Search from "@/components/ui/Search";
 import { CreatePost } from "./_/components/Buttons";
 import queryString from "query-string";
+import { getAllPostsApi } from "@/services/postService";
 
-function Page({ searchParams }) {
+async function Page({ searchParams }) {
   const query = queryString.stringify(searchParams);
+  const { totalPages } = await getAllPostsApi();
+
   return (
     <div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-secondary-800 mb-12 items-center">
@@ -17,6 +20,7 @@ function Page({ searchParams }) {
       <Suspense fallback={<Spinner />} key={query}>
         <PostsTable query={query} />
       </Suspense>
+      <div className="mt-5 flex w-full justify-center"></div>
     </div>
   );
 }
