392181e79ca4f6d90a301bccc78adb0b9df3c1eb
salarGholami <salargholami021@gmail.com>
2025-01-05 14:45:34 +0330

    Search Post

--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
 import PostsTable from "./_/components/PostsTable";
 import { Spinner } from "@/components/ui/Spinner";
+import Search from "@/components/ui/Search";
+import { CreatePost } from "./_/components/Buttons";
+import queryString from "query-string";
 
-function Page() {
+function Page({ searchParams }) {
+  const query = queryString.stringify(searchParams);
   return (
     <div>
-      <Suspense fallback={<Spinner />}>
-        <PostsTable />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-secondary-800 mb-12 items-center">
+        <h1 className="font-bold text-xl">لیست پست ها</h1>
+        <Search />
+        <CreatePost />
+      </div>
+      <Suspense fallback={<Spinner />} key={query}>
+        <PostsTable query={query} />
       </Suspense>
     </div>
   );
